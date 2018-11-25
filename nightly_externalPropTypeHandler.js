031db81c5ee44cec397660b6562538d28077e3c7
Benjamin Allen <ballen@nerdwallet.com>
2018-11-24 19:30:34 -0800

    all logic in the switch

--- nightly_externalPropTypeHandler.js
+++ nightly_externalPropTypeHandler.js
@@ -112,153 +112,6 @@ function resolveExternalIdentifier(path, filepath, options) {
   return path;
 }
 
-function resolveExternalsInArrayExpression(path, filepath, options) {
-  types.ArrayExpression.assert(path.node);
-  console.log('resolveExternalsInArrayExpression');
-
-  path.get('elements').each((elPath) => {
-    resolveExternals(elPath, filepath, options);
-  });
-  return path;
-}
-
-function resolveExternalsInCallExpression(path, filepath, options) {
-  types.CallExpression.assert(path.node);
-  console.log('resolveExternalsInCallExpression');
-
-  path.get('arguments').each((argPath) => {
-    resolveExternals(argPath, filepath, options);
-  });
-
-  return path;
-}
-
-function resolveExternalsInMemberExpression(path, filepath, options) {
-  types.MemberExpression.assert(path.node);
-
-  if (isPropTypesExpression(path)) {
-    (['object', 'property']).forEach((key) => {
-      const targetPath = path.get(key);
-      if (types.Identifier.check(targetPath.node)) {
-        return;
-      }
-      resolveExternals(targetPath, filepath, options);
-    });
-    return;
-  }
-  console.log('i am MemberExpression, not PropTypes, valuePath:', path);
-  throw new Error('UNHANDLED non-PropTypes MemberExpression');
-}
-
-function resolveExternalsInProperty(path, filepath, options) {
-  types.Property.assert(path.node);
-  console.log('resolveExternalsInProperty TODO: clean me up more');
-
-
-  const keyPath = path.get('key');
-  const valuePath = path.get('value');
-
-  // pretty sure this block is unnecessary
-  if (
-    !types.Identifier.check(keyPath.node)
-    && !types.Literal.check(keyPath.node)
-  ) {
-    // temp error
-    // console.log('property key should have been an identifier', propertyPath.get('key').value);
-    console.log('property key should have been an Identifier or Literal', path);
-    console.log('additional', filepath, path.get('key'));
-    throw new Error('property key was not an identifier')
-    return;
-  }
-
-  console.log('resolveExternalsInProperty path', keyPath.node.type);
-
-  // console.log('i am enum valuePath:', valuePath);
-  const memberExpressionRoot = getMemberExpressionRoot(valuePath);
-  // console.log('i am enum memberExpressionRoot', memberExpressionRoot);
-  const members = getMembers(valuePath);
-  // console.log('i am enum members', members[0] );
-
-  switch(valuePath.node.type) {
-    case types.Identifier.name: {
-      // console.log('Property value is Identifier', '\n\n', valuePath, '\n\n', valuePath.node.name);
-      // console.log('Property value is Identifier code\n', recast.print(valuePath).code, '\n\n', filepath);
-      // const resolvedValuePath = resolveIdentifierNameToExternalValue(valuePath.node.name, getRoot(path), filepath);
-      // valuePath.replace(resolvedValuePath.value);
-
-      // resolveExternalIdentifier(valuePath, filepath, options);
-      resolveExternals(valuePath, filepath, options);      
-      break;
-    }
-
-    case types.CallExpression.name: {
-      // console.log('i am enum or maybe shape valuePath:', valuePath);
-      // break;
-      const resolved = resolveExternals(valuePath, filepath, options);
-      // console.log('resolved i am enum or maybe shape valuePat', resolved);
-      break;
-    }
-
-    case types.MemberExpression.name: {
-      resolveExternals(valuePath, filepath, options);
-      // if (isPropTypesExpression(valuePath)) {
-      //   // .isRequired matched?
-      //   resolveExternals(valuePath.get('property'), filepath, options);
-      //   resolveExternals(valuePath.get('object'), filepath, options);
-      //   break;
-      // }
-      // console.log('i am MemberExpression, not PropTypes, valuePath:', valuePath);
-      // throw new Error('UNHANDLED non-PropTypes MemberExpression');
-      break;
-    }
-
-    default: {
-      console.log('UNHANDLED valuePath.node.type', valuePath.node.type);
-      throw new Error('should not have hit');
-      break;
-    }
-  }
-
-  amendPropType(path, options);
-
-  return path;
-}
-
-function resolveExternalsInSpreadProperty(path, filepath, options) {
-  types.SpreadProperty.assert(path.node);
-  console.log('resolveExternalsInSpreadProperty');
-
-  const argPath = path.get('argument');
-
-  switch(argPath.node.type) {
-    case types.Identifier.name: {
-      // const { path, ast, filepath } = resolveIdentifierNameToExternalValue(path.node.argument.name, getRoot(path), filepath);
-      // resolved = resolveIdentifierNameToExternalValue(path.node.argument.name, getRoot(path), filepath);
-      // resolveExternalIdentifier(argPath, filepath, options);
-      resolveExternals(argPath, filepath, options);      
-      break;
-    }
-    default: {
-      resolveExternals(argPath, filepath, options);
-    }
-  }
-
-  // overwrite the target value with the recursively processed ObjectExpression
-  // propPath.node.argument = resolvedObjectExpression.value;
-  types.ObjectExpression.assert(argPath.node);
-  return path;
-}
-
-function resolveExternalsInObjectExpression(path, filepath, options) {
-  types.ObjectExpression.assert(path.node);
-  console.log('resolveExternalsInObjectExpression');
-
-  path.get('properties').each((propertyPath) => {
-    resolveExternals(propertyPath, filepath, options);
-  });
-
-  return path;
-}
 
 function resolveExternals(path, filepath, options) {
   // console.log('resolveExternals', path);
@@ -273,36 +126,54 @@ function resolveExternals(path, filepath, options) {
   }
 
   switch(path.node.type) {
-    case types.Identifier.name: {
-      return resolveExternalIdentifier(path, filepath, options);
-    }
-    case types.ObjectExpression.name: {
-      return resolveExternalsInObjectExpression(path, filepath, options);
-    }
-    case types.ArrayExpression.name: {
-      return resolveExternalsInArrayExpression(path, filepath, options);
-    }
-    // TODO: do SpreadExpression so that path.node.value overwrite is not required
-    case types.SpreadProperty.name: {
-      // return resolveExternalsInSpreadProperty(path, filepath, options);
-      const resolved = resolveExternalsInSpreadProperty(path, filepath, options);
-      // path.get('argument').replace(resolved.value);
-      // path.node.argument = resolved.value;
-      return resolved;
+    case types.Literal.name: {
       break;
     }
     case types.Property.name: {
-      return resolveExternalsInProperty(path, filepath, options);
+      resolveExternals(path.get('value'), filepath, options);
+      amendPropType(path, options);
+      break;
+    }
+    case types.Identifier.name: {
+      return resolveExternalIdentifier(path, filepath, options);
+    }
+    case types.SpreadProperty.name: {
+      resolveExternals(path.get('argument'), filepath, options);
+      break;
+    }
+    case types.ObjectExpression.name: {
+      path.get('properties').each((propertyPath) => {
+        resolveExternals(propertyPath, filepath, options);
+      });
+      break;
+    }
+    case types.ArrayExpression.name: {
+      path.get('elements').each((elPath) => {
+        resolveExternals(elPath, filepath, options);
+      });
+      break;
     }
     case types.CallExpression.name: {
-      return resolveExternalsInCallExpression(path, filepath, options);
+      path.get('arguments').each((argPath) => {
+        resolveExternals(argPath, filepath, options);
+      });
+      break;
     }
     case types.MemberExpression.name: {
-      return resolveExternalsInMemberExpression(path, filepath, options);
-    }
-    // case types.Identifier.name:
-    case types.Literal.name: {
-      return path;
+      if (isPropTypesExpression(path)) {
+        (['object', 'property']).forEach((key) => {
+          const targetPath = path.get(key);
+          if (types.Identifier.check(targetPath.node)) {
+            return;
+          }
+          resolveExternals(targetPath, filepath, options);
+        });
+      } else {
+        console.log('i am MemberExpression, not PropTypes, valuePath:', path);
+        // TODO: temp throw erropr
+        throw new Error('UNHANDLED non-PropTypes MemberExpression');
+      }
+      break;
     }
     default: {
       console.log('resolveExternals UNHANDLED', path.node.type);
@@ -310,6 +181,8 @@ function resolveExternals(path, filepath, options) {
       throw new Error('resolveExternals UNHANDLED: ' + path.node.type);
     }
   }
+
+  return path;
 }
 
 
@@ -355,7 +228,7 @@ function getExternalPropTypeHandler(propName) {
       const resolved = resolveExternals(propTypesPath, filepath, { getDescriptor, documentation });
 
 
-      console.log(recast.print(propTypesPath).code);
+      // console.log(recast.print(propTypesPath).code);
 
     };
   }
