535c3ced8e6cc51ac0c08935eb2d76fa0b3363cf
Benjamin Allen <ballen@nerdwallet.com>
2018-11-25 13:28:20 -0800

    add the fixtures used for last nights module.exports dev

---
+++ tests/fixtures/api-cjs-named-exports.js
@@ -0,0 +1,22 @@
+const PropTypes = require('prop-types');
+
+const propTypes = {
+  /**
+   * Comment for cjs-api-named-exports-node-required.
+   */
+  'cjs-api-named-exports-node-required': PropTypes.node.isRequired,
+  /**
+   * Comment for cjs-api-named-exports-string.
+   */
+  'cjs-api-named-exports-string': PropTypes.string,
+};
+
+const defaultProps = {
+  'cjs-api-named-exports-string': 'cjs-api-named-exports-string-value',
+};
+
+module.exports.propTypes = propTypes;
+module.exports.defaultProps = defaultProps;
+// module.exports = { propTypes, defaultProps };
+
+// module.exports.defaultProps__ = defaultProps;
