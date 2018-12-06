6a17142a03dac65c5e47f1613f4496851baec163
Benjamin Allen <ballen@nerdwallet.com>
2018-12-05 17:48:42 -0800

    add more types to api-external-values

--- src/handlers/__tests__/fixtures/components/assets/api-external-values.js
+++ src/handlers/__tests__/fixtures/components/assets/api-external-values.js
@@ -18,7 +18,48 @@ export const propTypes = {
    */
   'api-external-values-shape-with-enum': PropTypes.shape({
     enum: PropTypes.oneOf([STRING, NUMBER, BOOL]),
+    bool: PropTypes.bool.isRequired,
   }).isRequired,
+  /**
+   * Comment for api-external-values-arrayOf-enum.
+   */
+  'api-external-values-arrayOf-enum': PropTypes.arrayOf(
+    PropTypes.oneOf([STRING, NUMBER, BOOL]),
+  ).isRequired,
+  /**
+   * Comment for api-external-values-oneOfType-with-enum-and-shape.
+   */
+  'api-external-values-oneOfType-with-enum-and-shape': PropTypes.oneOfType([
+    PropTypes.oneOf([STRING, NUMBER, BOOL]),
+    PropTypes.shape({
+      enum1: PropTypes.oneOf([STRING, NUMBER, BOOL]),
+      number1: PropTypes.number.isRequired,
+    }),
+  ]).isRequired,
+  /**
+   * Comment for api-external-values-objectOf-enum.
+   */
+  'api-external-values-objectOf-enum': PropTypes.objectOf(
+    PropTypes.oneOf([STRING, NUMBER, BOOL]),
+  ).isRequired,
+  /**
+   * api-external-values-objectOf-shape-with-enum.
+   */
+  'api-external-values-objectOf-shape-with-enum': PropTypes.objectOf(
+    PropTypes.shape({
+      enum2: PropTypes.oneOf([STRING, NUMBER, BOOL]),
+      number2: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+  /**
+   * Comment for api-external-values-arrayOf-enum.
+   */
+  'api-external-values-arrayOf-shape-with-enum': PropTypes.arrayOf(
+    PropTypes.shape({
+      enum3: PropTypes.oneOf([STRING, NUMBER, BOOL]),
+      number3: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
   /**
    * Comment for api-external-values-notAnEnum.
    */
